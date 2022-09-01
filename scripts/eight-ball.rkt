df6886a6730c4d1cfaa4218c7408323312435581
comeillfoo <rayveleaveme34@inbox.ru>
2022-09-01 22:30:23 +0100

    fix: formatting for eight-ball

--- scripts/eight-ball.rkt
+++ scripts/eight-ball.rkt
@@ -1,7 +1,12 @@
 #lang racket
 
-(let ([answers (list "Yes" "No" "Yes" "No doubt" "Very doubtful" "Try again later" "Reply hazy and try again")])
+(let ([answers (list "Yes"
+                     "No"
+                     "Yes"
+                     "No doubt"
+                     "Very doubtful"
+                     "Try again later"
+                     "Reply hazy and try again")])
   (write-string "Please ask yes/no question:\n")
   (read-line)
-  (void (write-string (string-append (list-ref answers (random (length answers))) "\n")))
-)
+  (void (write-string (string-append (list-ref answers (random (length answers))) "\n"))))
